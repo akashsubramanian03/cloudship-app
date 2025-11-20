ff2bdb3bb70241de59fffd057256fa8a320c5251
akashsubramanian03 <akashsubbu03@gmail.com>
2025-11-21 04:38:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CloudShip CI/CD Pipeline is running — UPDATED !"
+    return "CloudShip CI/CD Pipeline is running — UPDATEDDD !"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
